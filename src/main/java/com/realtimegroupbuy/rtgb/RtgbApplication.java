dc16becc299de643b64dfaaafd12f0eaa2320f3c
Sangkwon Lee <sangkwon26@gmail.com>
2024-09-02 17:49:13 +0900

    init: project

---
+++ src/main/java/com/realtimegroupbuy/rtgb/RtgbApplication.java
@@ -0,0 +1,13 @@
+package com.realtimegroupbuy.rtgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtgbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realtimegroupbuy/rtgb/RtgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimegroupbuy.rtgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
