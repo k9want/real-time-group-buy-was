e08cb31c07a48fa5c6bf80fbed55185a682b9b20
Sangkwon Lee <sangkwon26@gmail.com>
2024-09-02 18:46:52 +0900

    feat: 사용자 조회, 생성, 삭제 기능 개발

---
+++ src/main/java/com/realtimegroupbuy/rtgb/service/UserService.java
@@ -0,0 +1,27 @@
+package com.realtimegroupbuy.rtgb.service;
+
+import com.realtimegroupbuy.rtgb.model.User;
+import com.realtimegroupbuy.rtgb.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("User Not Found"));
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
