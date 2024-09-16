f3340c8a4122c35012744fdf75e32b5d5c4b6cf2
Sangkwon Lee <sangkwon26@gmail.com>
2024-09-16 17:10:52 +0900

    update: User 도메인 테이블명 users로 변경

--- src/main/java/com/realtimegroupbuy/rtgb/model/User.java
+++ src/main/java/com/realtimegroupbuy/rtgb/model/User.java
@@ -8,12 +8,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
